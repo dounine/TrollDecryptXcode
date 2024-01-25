afbcd50f4591606fde156577007db5a9b50095cf
lake <102535481@qq.com>
2024-01-25 10:37:33 +0800

    init

--- TDFileManagerViewController.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface TDFileManagerViewController : UITableViewController
-
-@property (nonatomic, strong) NSArray *fileList;
-
-@end
--- TDRootViewController.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface TDRootViewController : UITableViewController
-
-@property (nonatomic, strong) NSArray *apps;
-
-@end
